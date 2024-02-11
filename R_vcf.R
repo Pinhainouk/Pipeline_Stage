d2ca81be2c78292803c66a5350966b9b2eaac02d
Pinhainouk <elodieparis0@gmail.com>
2024-02-11 12:07:36 +0100

    MAJ R

--- R_vcf.R
+++ R_vcf.R
@@ -192,3 +192,210 @@ plot3 = ggplot(data2, aes(fill=Type_Variants, y=Nombre_Variants, x=Nom_VCF))+geo
 library(plotly)
 ggplotly(plot3)
 
+#Idem à l'horizontal
+plot4 = ggplot(data2, aes(fill=Type_Variants, y=Nom_VCF, x=Nombre_Variants))+geom_bar(position = "stack", stat="identity")
+ggplotly(plot4)
+
+#Nombre de variants par VCF et par nom de gènes
+nbGene = vcf_bind %>% group_by (vcf_bind$Gene, vcf_bind$VCF) %>% count(vcf_bind$Type)
+data3=data_frame(nbGene)
+names(data3)[1]="Nom_Genes"
+names(data3)[2]="Nom_VCF"
+names(data3)[3]="Type_Variants"
+names(data3)[4]="Nombre_Variants"
+plot5 = ggplot(data3, aes(fill=Nom_Genes, y=Nombre_Variants, x=Nom_VCF))+geom_bar(position = "stack", stat="identity")
+ggplotly(plot5)
+
+data3$NomVcf_NomGene = paste(data3$Nom_VCF,data3$Nom_Genes, sep="_")
+plot6 = ggplot(data3, aes(fill=Type_Variants, y=Nombre_Variants, x=NomVcf_NomGene))+geom_bar(position = "fill", stat="identity")
+plot6 = ggplot(data3, aes(fill=Type_Variants, y=Nombre_Variants, x=NomVcf_NomGene))+
+  geom_bar(position = position_fill(reverse=TRUE), stat="identity")+
+  labs(title = "Représentation graphique du nombre de variants en fonction des gènes et des VCF par types de variants") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  ggplotly(plot6)
+
+cat_split %>% filter(Catégorie == "cat_ref")
+cat_split %>% filter(Catégorie == "cat_12x")
+
+#compter la taille des insertions et des délétions
+#dataframe avec que délétions, insertions ou snp
+vcf_deletion = vcf_bind %>% filter(Type == "délétion")
+vcf_insertion = vcf_bind %>% filter(Type=="insertion")
+vcf_snp = vcf_bind %>% filter(Type=="SNP")
+
+vcf_deletion$Taille_delins = (nchar(vcf_deletion$ref)-1)
+vcf_insertion$Taille_delins = (nchar(vcf_insertion$alt)-1)
+
+cat_split %>% filter(position=="66440385")
+
+#09/02/2024
+
+# Représentation en boxplot des délétions
+box_plotVCF <- ggplot(vcf_deletion, aes(x = VCF, y = Taille_delins)) +
+  geom_boxplot(outlier.colour = "#756bb1", outlier.shape = 2, outlier.size = 3) +
+  geom_point(stat = "summary", fun = "mean", size = 3, color = "#2ca25f") +
+  coord_flip() +
+  theme_classic() +
+  labs(x = "VCF", y = "Taille des délétions", title = "Représentation en boxplot de la taille des délétions par VCF") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+
+box_plotVCF
+
+box_plotGene <- ggplot(vcf_deletion, aes(x = Gene, y = Taille_delins)) +
+  geom_boxplot(outlier.colour = "#756bb1", outlier.shape = 2, outlier.size = 3) +
+  geom_point(stat = "summary", fun = "mean", size = 3, color = "#2ca25f") +
+  coord_flip() +
+  theme_classic() +
+  labs(x = "Gènes", y = "Taille des délétions", title = "Représentation en boxplot de la taille des délétions par gènes") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+
+box_plotGene
+
+#Représentation en boxplot des insertions
+box_plotinsVCF <- ggplot(vcf_insertion, aes(x = VCF, y = Taille_delins)) +
+  geom_boxplot(outlier.colour = "#d95f0e", outlier.shape = 2, outlier.size = 3) +
+  geom_point(stat = "summary", fun = "mean", size = 3, color = "#2b8cbe") +
+  coord_flip() +
+  theme_classic() +
+  labs(x = "VCF", y = "Taille des insertions", title = "Représentation en boxplot de la taille des insertions par VCF") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+
+box_plotinsVCF
+
+box_plotinsGene <- ggplot(vcf_insertion, aes(x = Gene, y = Taille_delins)) +
+  geom_boxplot(outlier.colour = "#d95f0e", outlier.shape = 2, outlier.size = 3) +
+  geom_point(stat = "summary", fun = "mean", size = 3, color = "#2b8cbe") +
+  coord_flip() +
+  theme_classic() +
+  labs(x = "Gènes", y = "Taille des délétions", title = "Représentation en boxplot de la taille des insertions par gènes") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+
+box_plotinsGene
+
+#Renommer les colonnes pour faire un rbind des VCF délétions et insertions en vue de faire un boxplot avec les 2 représentations
+colnames(vcf_deletion)[8] = c("Taille_delins")
+colnames(vcf_insertion)[8] = c("Taille_delins")
+vcfdelins = rbind(vcf_insertion, vcf_deletion)
+
+#Représentation en boxplot des délétions et insertions par VCF
+boxplotVCF_delins = ggplot(vcfdelins, aes(x = Taille_delins, y = VCF)) + geom_boxplot(aes(fill=Type)) + theme_classic() + 
+  labs(x = "Taille des delins", y = "VCF", title = "Représentation en boxplot de la taille des insertions et délétions par VCF") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+
+#Représentation en boxplot des délétions et insertions par gènes
+boxplotGene_delins = ggplot(vcfdelins, aes(x = Taille_delins, y = Gene)) + geom_boxplot(aes(fill=Type)) + theme_classic() + 
+  labs(x = "Gènes", y = "Taille des delins", title = "Représentation en boxplot de la taille des insertions et délétions par Gènes") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+  
+  
+#Représentation des délétions par gène et par VCF
+  box_plotdelGene_VCF <- ggplot(vcf_deletion, aes(x = VCF, y = Taille_delins)) +
+  geom_boxplot(aes(fill=Gene)) +
+  geom_point(stat = "summary", fun = "mean", size = 3, color = "#2ca25f")+
+  theme_classic() +
+  labs(x = "VCF", y = "Taille des délétions", title = "Représentation en boxplot de la taille des délétions par gènes et VCF") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+
+box_plotdelGene_VCF
+
+box_plotdelVCF_Gene <- ggplot(vcf_deletion, aes(x = Gene, y = Taille_delins)) +
+  geom_boxplot(aes(fill=VCF)) +
+  geom_point(stat = "summary", fun = "mean", size = 3, color = "#2ca25f")+
+  theme_classic() +
+  labs(x = "Gènes", y = "Taille des délétions", title = "Représentation en boxplot de la taille des délétions par gènes et VCF") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+
+box_plotdelVCF_Gene
+
+box_plotinsGene_VCF <- ggplot(vcf_insertion, aes(x = Gene, y = Taille_delins)) +
+  geom_boxplot(outlier.colour = "#d95f0e", outlier.shape = 2, outlier.size = 3) +
+  geom_point(stat = "summary", fun = "mean", size = 3, color = "#2b8cbe") +
+  coord_flip() +
+  theme_classic() +
+  labs(x = "Gènes", y = "Taille des insertions", title = "Représentation en boxplot de la taille des insertions par gènes") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+
+box_plotinsGene_VCF
+
+#Représentation des insertions par gène et par VCF
+box_plotinsGene_VCF <- ggplot(vcf_insertion, aes(x = VCF, y = Taille_delins)) +
+  geom_boxplot(aes(fill=Gene)) +
+  geom_point(stat = "summary", fun = "mean", size = 3, color = "#2b8cbe") +
+  coord_flip() +
+  theme_classic() +
+  labs(x = "VCF", y = "Taille des insertions", title = "Représentation en boxplot de la taille des insertions par gènes") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+ggplotly(box_plotinsGene_VCF)
+
+box_plotinsVCF_Gene <- ggplot(vcf_insertion, aes(x = Gene, y = Taille_delins)) +
+  geom_boxplot(aes(fill=VCF)) +
+  geom_point(stat = "summary", fun = "mean", size = 3, color = "#2b8cbe") +
+  coord_flip() +
+  theme_classic() +
+  labs(x = "Gènes", y = "Taille des insertions", title = "Représentation en boxplot de la taille des insertions par gènes") + 
+  theme(plot.title = element_text(hjust = 0.5, size=18, face="bold"), plot.subtitle = element_text(hjust = 0.5))
+box_plotinsVCF_Gene
+
+#Voir si présence d'inversions
+vcfdelins %>% filter(nchar(ref) == nchar(alt))
+
+#Transitions/transversions - ajout colonne dans le df vcf_snp
+vcf_snp$Type_substitutions = ifelse (vcf_snp$ref=="A" & vcf_snp$alt=="G", "transition_A>G",
+                                     ifelse (vcf_snp$ref=="G" & vcf_snp$alt=="A", "transition_G>A",
+                                            ifelse(vcf_snp$ref=="C" & vcf_snp$alt=="T", "transition_C>T",
+                                                   ifelse(vcf_snp$ref=="T" & vcf_snp$alt=="C", "transition_T>C",
+                                                          ifelse(vcf_snp$ref=="A" & vcf_snp$alt=="C", "transversion_A>C",
+                                                                 ifelse(vcf_snp$ref=="C" & vcf_snp$alt=="A", "transversion_C>A",
+                                                                        ifelse(vcf_snp$ref=="G" & vcf_snp$alt=="T", "transversion_G>T",
+                                                                               ifelse(vcf_snp$ref=="T" & vcf_snp$alt=="G", "transversion_T>G",
+                                                                                      ifelse(vcf_snp$ref=="A" & vcf_snp$alt=="T", "transversion_A>T",
+                                                                                             ifelse(vcf_snp$ref=="T" & vcf_snp$alt=="A", "transversion_T>A",
+                                                                                                    ifelse(vcf_snp$ref=="G" & vcf_snp$alt=="C", "transversion_G>C",
+                                                                                                           ifelse(vcf_snp$ref=="C" & vcf_snp$alt=="G", "transversion_C>G","autre"))))))))))))
+
+
+#Représentation des transitions/transversions
+nb_substitutions=vcf_snp %>% group_by (Gene, VCF, Type_substitutions) %>% summarise(Nombre_substitutions=n())
+
+data_subs=data_frame(nb_substitutions)
+names(data_subs)[1]="Nom_Gene"
+names(data_subs)[2]="VCF"
+names(data_subs)[3]="Type_Substitutions"
+names(data_subs)[4]="Nombre_Substitutions"
+
+# Nombre de types de substitutions par type et par gène
+plot_subs <- ggplot(data_subs, aes(fill = Nom_Gene, y = Nombre_Substitutions, x = Type_Substitutions)) +
+  geom_bar(position = "stack", stat = "identity") +
+  labs(title = "Nombre de substitutions par type et par gène", x = "Type de substitutions", y = "Nombre de substitutions") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+plot_subs
+
+# Nombre de types de substitutions_ref>alt par type et par VCF
+plot_subs2 <- ggplot(data_subs, aes(fill = VCF, y = Nombre_Substitutions, x = Type_Substitutions)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  labs(title = "Nombre de substitutions par type et par VCF", x = "Type de substitutions", y = "Nombre de substitutions") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+plot_subs2
+
+#Création d'un nouveau DF avec 2 colonnes supplémentaires : Type de substitutions et ref>alt séparé
+data_subs2 = data_subs
+data_subs2 = data_subs2 %>% separate(Type_Substitutions, c("Type", "ref>alt"), "_")
+
+#barplot nombre de subtitutions et types par VCF
+plot_subs3 <- ggplot(data_subs2, aes(fill = Type, y = Nombre_Substitutions, x = VCF)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  labs(title = "Nombre de substitutions par type et par VCF", x = "Type de substitutions", y = "Nombre de substitutions") +
+  theme_minimal()
+plot_subs3
+
+#barplot nombre de subtitutions et types par gènes
+plot_subs4 <- ggplot(data_subs2, aes(fill = Type, y = Nombre_Substitutions, x = Nom_Gene)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  labs(title = "Nombre de substitutions par type et par gènes", x = "Type de substitutions", y = "Nombre de substitutions") +
+  theme_minimal()
+plot_subs4
+
+
+
